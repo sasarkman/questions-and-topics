c2de2d8c01e2fc0f41a6b93cfac7c122066d18bd
Sasa Rkman <sasarkman@u.boisestate.edu>
2022-03-17 14:29:22 -0600

    - Finish implementing and testing /search route

--- app.js
+++ app.js
@@ -25,55 +25,72 @@ const { query, validationResult } = require('express-validator');
 mongoose.connect(mongo_url, { useNewUrlParser:true, useUnifiedTopology:true }, (err) => {
 	if (err) {
 		console.log(err);
-		console.log("Could not connect to database, exiting...");
+		console.log('Could not connect to database, exiting...');
 		process.exit(0);
 	}
-	console.log("Connected to database: " + mongo_url);
+	console.log('Connected to database: ' + mongo_url);
 });
 
 const Questions = require('./models/questions-model');
 const Topics = require('./models/topics-model');
-app.get('/search', query('q').notEmpty(), function(req, res, next) {
+app.get('/search', query('q').notEmpty(), function(req, res) {
 	const errors = validationResult(req);
 	if(!errors.isEmpty()) {
-		return res.status(400).json({ msg: 'Missing name of topic in query' });
+		return res.status(400).send('Please specify a topic in the "q" query string');
 	}
 
 	const query = req.query.q;
 
-	Topics.aggregate([
-		{
-			$match: {
-				"Topic Level 1": query
-			}
-		},
-		{
-			$lookup: {
-				from: 'questions',
-				localField: 'Topic Level 2',
-				foreignField: 'Annotation 1',
-				as: 'question'
-			}
-		},
-		{ 
-			$unwind: '$question' 
-		},
-		{
-			$project: {
-				'_id': 0,
-				'question.Question number': 1
-			}
-		},
-		
-		], function(err, result) {
-			if(err || !result.length) res.status(400).json({ msg: 'No such topic found'})
-			else {
-				const output = result.map(question => question.question['Question number']);
-				return res.status(200).json(output);
-			}
+	// Get all topics whose Topic Level 1 match the query provided
+	Topics.find({'Topic Level 1': query}, { '_id': 0, 'Topic Level 2': 1, 'Topic Level 3': 1}, function(err, docs) {
+		// Based on topic level 1, compile a list of all unique topics from level 2 and 3
+		var targetTopics = []
+		for(var topics in docs) {
+			if(docs[topics]['Topic Level 2'] != '' && !targetTopics.includes(docs[topics]['Topic Level 2'])) targetTopics = targetTopics.concat(docs[topics]['Topic Level 2']);
+			if(docs[topics]['Topic Level 3'] != '' && !targetTopics.includes(docs[topics]['Topic Level 3'])) targetTopics = targetTopics.concat(docs[topics]['Topic Level 3']);
 		}
-	);
 
+		// Find all questions whose annotations contain any of our target topics
+		Questions.find(
+			{
+				$or: [
+					{'Annotation 1': {$in: targetTopics}},
+					{'Annotation 2': {$in: targetTopics}},
+					{'Annotation 3': {$in: targetTopics}},
+					{'Annotation 4': {$in: targetTopics}},
+					{'Annotation 5': {$in: targetTopics}},
+				]
+			},
+			// Define specific columns we want returned
+			{ 
+				"_id": 0, 
+				"Question number": 1, 
+				"Annotation 1": 1, 
+				"Annotation 2": 1, 
+				"Annotation 3": 1, 
+				"Annotation 4": 1, 
+				"Annotation 5": 1
+			},
+			function(err, docs) {
+				if(err || !docs.length) res.status(400).send('No such topic found')
+				else {
+					var cleanedQuestions = [];
+
+					// Remove questions whose annotations are not a sub-topic of our Level 1 topic
+					docs.forEach(element => {
+						if(!targetTopics.includes(element['Annotation 1']) && element['Annotation 1'] != "") return;
+						if(!targetTopics.includes(element['Annotation 2']) && element['Annotation 2'] != "") return;
+						if(!targetTopics.includes(element['Annotation 3']) && element['Annotation 3'] != "") return;
+						if(!targetTopics.includes(element['Annotation 4']) && element['Annotation 4'] != "") return;
+						if(!targetTopics.includes(element['Annotation 5']) && element['Annotation 5'] != "") return;
+						cleanedQuestions = cleanedQuestions.concat(element['Question number']);
+					})
+
+					return res.status(200).send(cleanedQuestions);
+				}
+			}
+		);
+	})
 });
 
 // Express IP and port info
